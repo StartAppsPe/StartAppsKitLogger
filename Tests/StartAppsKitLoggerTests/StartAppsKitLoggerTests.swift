d42a2e05877762212c17ffa3ab7466f02bf48131
Gabriel Lanata <gabriellanata@gmail.com>
2017-09-15 13:00:18 -0500

    Update Swift 4

--- Tests/StartAppsKitLoggerTests/StartAppsKitLoggerTests.swift
+++ Tests/StartAppsKitLoggerTests/StartAppsKitLoggerTests.swift
@@ -2,16 +2,15 @@ import XCTest
 @testable import StartAppsKitLogger
 
 class StartAppsKitLoggerTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(StartAppsKitLogger().text, "Hello, World!")
+    
+    func testLogInfo() {
+        Log.info("Tester","Here is some value","Another value")
     }
-
 
     static var allTests : [(String, (StartAppsKitLoggerTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testLogInfo", testLogInfo),
         ]
     }
+    
 }
